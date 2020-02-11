febb5b8780c24dd07ba18516e44cf8e18ae1a1e8
Robert <bibbycodes@gmail.com>
2020-02-11 11:14:41 +0000

    added travis

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path')
 app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Express Template')
 })
 
 app.listen(PORT, () => {
