0fb2540afb2175a3bc87136a6c986ac713fe307e
Robert <bibbycodes@gmail.com>
2020-02-11 11:08:23 +0000

    template server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const PORT = process.env.HTTP_PORT || 5000;
+const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}.`)
+})
